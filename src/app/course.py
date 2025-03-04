d61a66913b89555561359236a28304b0a7d32a21
Moses Oghene <mosesogheneo@gmail.com>
2025-03-04 16:27:18 +0100

    Feature: Student and Instructor can be created and verified

--- src/app/course.py
+++ src/app/course.py
@@ -1,9 +1,7 @@
-from app.user import Student, Instructor
-
 
 class Course:
     def __init__(self, course_code: str, course_name:str):
         self._course_code: str = course_code
         self._course_name = course_name
-        self._instructor: [Instructor, None] = None
-        self._students: [Student] = []
+        self._instructor: ['Instructor', None] = None
+        self._students: ['Student'] = []
--- src/app/user.py
+++ src/app/user.py
@@ -1,22 +1,48 @@
-from course import Course
+from abc import ABC
+
+from app.course import Course
 
 
-class User:
+class User(ABC):
     def __init__(self, email: str, password: bytes):
+        self._name = None
         self._email = email
         self._password = password
         self._is_authenticated = False
 
+    @property
+    def email(self):
+        return self._email
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name: str):
+        self._name = name
+
+
+
 
 class Student(User):
-    pass
+    def __init__(self, email: str, password: bytes):
+        super().__init__(email, password)
+        self._courses: ['Course'] = []
+
+    def is_student(self):
+        return isinstance(self, Student)
 
 
 class Instructor(User):
     def __init__(self, email: str, password: bytes):
         super().__init__(email, password)
-        self._courses: [Course] = []
-        self._enrolled_students: [Student] = []
+        self._courses: ['Course'] = []
+        self._enrolled_students: ['Student'] = []
+
+    def is_instructor(self):
+        return isinstance(self, Instructor)
+
 
 
 
