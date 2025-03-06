339651a343c79f29687ebfdf68793e58905ff4ab
0d-fako <od.fakorede@gmail.com>
2025-03-06 08:04:02 +0100

    Bug fixes

--- src/app/instructor.py
+++ src/app/instructor.py
@@ -1,25 +1,33 @@
+from typing import List
+
 from src.app.course import Course
 from src.app.student import Student
+from src.app.user import User
 
-
-class Instructor:
-    def __init__(self, first_name, last_name, email, password):
-        super().__init__(first_name, last_name, email, password)
+class Instructor(User):
+    def __init__(self, first_name: str, last_name: str, email: str, password: str):
+        super().__init__(email, password, first_name, last_name)
         self.__teaching_courses = []
 
-    def create_course(self, course: 'Course'):
-        if course in self.__teaching_courses: raise ValueError('Course already exists')
+    def create_course(self, course: Course):
+        if course in self.__teaching_courses:
+            raise ValueError('Course already exists')
         self.__teaching_courses.append(course)
 
-    def view_students_in_course(self, course: Course):
+    def view_teaching_courses(self) -> List[Course]:
+        return self.__teaching_courses
+
+    def view_students_in_course(self, course: Course) -> List[Student]:
+        if course not in self.__teaching_courses:
+            raise ValueError("You are not teaching this course")
         return course.get_students()
 
-    def assign_grade(self, student:Student):
-        pass
+    def assign_grade(self, student: Student, course: Course, grade: float):
+        if course not in self.__teaching_courses:
+            raise ValueError("You are not teaching this course")
+        if student not in course.get_students():
+            raise ValueError("Student is not enrolled in this course")
+        course.set_student_grade(student, grade)
 
-
-    def set_course(self, course):
-        self.__teaching_courses = course
-
-    def get_course(self):
+    def get_course(self) -> List[Course]:
         return self.__teaching_courses
