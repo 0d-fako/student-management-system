591211a69d9190d0cf5d6fb82e3e3ffa1ff15c24
Moses Oghene <mosesogheneo@gmail.com>
2025-03-12 05:13:03 +0100

    Folder-Refactor

--- views/__init__.py
+++
@@ -1,5 +0,0 @@
-from .instructor_view import InstructorDashboard
-from .student_view import StudentDashboard
-from .home_view import MainMenu
-
-__all__ = ['InstructorDashboard', 'StudentDashboard', 'MainMenu']
